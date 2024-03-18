a346340444457a4fc5dcbe611f713321c6cbfcb7
Vishnu M <vishnumurali7000@gmail.com>
2024-03-19 00:28:00 +0530

    Add rake task to print average wait times grouped by concurrency_level

--- app/models/gvlwait/duration.rb
+++ app/models/gvlwait/duration.rb
@@ -2,5 +2,9 @@ module Gvlwait
   class Duration < ApplicationRecord
     validates :process_type, presence: true, inclusion: { in: ['web', 'worker'] }
     validates :concurrency_level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+    def self.average_wait_times_by_concurrency
+      group(:concurrency_level).average(:wait_duration)
+    end
   end
 end
