090936314405fa980f9ffc76e70f83ec0c465b46
Vishnu M <vishnumurali7000@gmail.com>
2024-03-20 11:59:28 +0530

    Configure puma max_threads from the host application itself

--- lib/gvlwait.rb
+++ lib/gvlwait.rb
@@ -1,7 +1,18 @@
 require "gvlwait/version"
 require "gvlwait/engine"
 require "gvlwait/gvl_instrumentation_middleware"
+require "gvlwait/configuration"
 
 module Gvlwait
-  # Your code goes here...
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
 end
